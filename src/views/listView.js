9b69858f3c87ce1347f2b0cc176892c36983613e
ptnoire <ptnoire@gmail.com>
2023-02-10 15:37:03 -0500

    Updated some CSS just cause

--- src/views/listView.js
+++ src/views/listView.js
@@ -17,13 +17,12 @@ class ListView extends View {
     }
 
     _generateMarkup(data) {
-        // console.log(data);
         return `
         <li class="bill__item">
             <h1>${data.bill.name}</h1>
             <h2>Amount Due: ${data.bill.amount}</h2>
             <h3>Due Date: ${data.bill.dueDate}</h3>
-            <h3>${data.bill.reoccuring ? '' : 'Reoccuring Bill'}</h3>
+            <h3>${data.bill.reoccuring ? 'Reoccuring Bill' : ''}</h3>
             <button>Payd!</button>
         </li>
         `
